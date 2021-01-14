2740fcb80e372205a2732cdca82e8098874fdf7b
Austin McCombs <austinmccombs22@gmail.com>
2021-01-13 20:26:18 -0700

    Dev tool initial commit

---
+++ lib/company_view_tool.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require_relative "company_view_tool/version"
+require_relative "company_view_tool/renderer
+"
+
+module CompanyViewTool
+  class Error < StandardError; end
+  # Your code goes here...
+end
